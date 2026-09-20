b290df2cc0fd36a2c191d4714b90f0272c3b79f8
Ishuman <ishuman2006@gmail.com>
2026-09-20 16:35:10 +0530

    test: continued tests for CLI creation, scratch.py

--- non_lib/cli.py
+++ non_lib/cli.py
@@ -4,18 +4,24 @@ def raise_power(base: float, power: float) -> float:
     """Raises a given base to a given power"""
     return base**power
 
-parser = argparse.ArgumentParser()
-parser.add_argument("square", help="display the square of a given number", type=int)
-# parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true") # store_true means assign the value True to args.verbose, not specifying this option implies False. Thus, this is an OPTIONAL argument.
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    # we will add conflicting options quiet and verbose here
+    parser.add_argument("x", type=float, help="the base")
+    parser.add_argument("y", type=float, help="the exponent")
+    parser.add_argument("-v", "--verbosity", type=int, choices=[0, 1, 2], default=0, help="the verbosity level you want the program to output at")
+    args = parser.parse_args()
+    user_base = args.x
+    user_exp = args.y
+    answer = raise_power(user_base, user_exp)
+    if args.verbosity == 2:
+        print(f"You have set the verbosity level to {args.verbosity}")
+        print(f"{user_base} raised to {user_exp} gives {answer}")
+    elif args.verbosity == 1:
+        print(f"You have set the verbosity level to {args.verbosity}")
+        print(f"{user_base}**{user_exp} == {answer}")
+    else:
+        print(f"You have not set the verbosity level to anything, so it's at {args.verbosity}")
+        print(answer)
 
-parser.add_argument("-v", "--verbosity", help="increase output verbosity", type=int, choices=[0, 1, 2], default=0) # note that you cannot use store_true when you're making the option have some type such as int. store_true only means "if this flag was supplied, store the value true in args.verbose"
 
-args = parser.parse_args()
-if args.verbosity == 2:
-    print(f"you set the verbosity level to {args.verbosity}")
-    print(f"the square of the given number is {args.square**2}")
-elif args.verbosity == 1:
-    print(f"you set the verbosity level to {args.verbosity}")
-    print(f"{args.square}^2 == {args.square**2}")
-else:
-    print(args.square**2)
